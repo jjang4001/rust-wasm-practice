7a150fb14c2d84fafb3b9edab794ec1ffe44a960
Junwon Jang <junwonjang2018@u.northwestern.edu>
2019-01-26 21:52:02 -0600

    use relative imports instead of from npm packages

--- www/bootstrap.js
+++ www/bootstrap.js
@@ -1,5 +1,9 @@
 // A dependency graph that contains any wasm must all be imported
 // asynchronously. This `bootstrap.js` file does the single async import, so
 // that no one else needs to worry about it again.
+
 import("./index.js")
   .catch(e => console.error("Error importing `index.js`:", e));
+
+import("./game-of-life.js")
+  .catch(e => console.error("Error importing `index2.js`:", e));
